b98b837e110b27ead06f05783686f96c19c71af9
NT-Malware <150932842+NT-Malware@users.noreply.github.com>
2023-11-22 12:03:27 +0500

    Add files via upload

---
+++ Math.py
@@ -0,0 +1,20 @@
+m=int(input("enter value"))
+import math
+r=math.sqrt(m)
+print(r)
+
+m=int(input("enter value"))
+import math
+r=math.pow(m,2)
+print(r)
+
+m=int(input("enter value"))
+import math
+r=math.tan(m)
+print(r)
+
+m=int(input("enter value"))
+import math
+r=math.log(m)
+print(r)
+
