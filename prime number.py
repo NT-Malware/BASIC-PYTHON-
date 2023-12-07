d6cbe1a4355d24b0e43cfba1c57d204b63491aa5
NT-Malware <150932842+NT-Malware@users.noreply.github.com>
2023-12-07 19:33:54 +0500

    Add files via upload

---
+++ prime number.py
@@ -0,0 +1,11 @@
+#check weather is prime number or not
+num=int(input("enter value"))
+if num > 1:
+    for i in range(2,num):
+        if (num%i)==0:
+            print("not a prime number")
+            break
+        else:
+            print("prime number")
+else:
+    print("not a prime number")
